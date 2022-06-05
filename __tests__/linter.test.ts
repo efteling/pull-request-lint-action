3bcef86b9bb75c6b874bbe0f1c05a240de0db455
Patrick Kempff <patrickkempff@gmail.com>
2022-06-05 14:38:44 +0200

    Add extra test

--- __tests__/linter.test.ts
+++ __tests__/linter.test.ts
@@ -1,4 +1,5 @@
 import lint, {LintRule} from '../src/linter'
+import {PullRequestContext} from '../src/utils'
 import * as core from '@actions/core'
 
 jest.mock('@actions/core')
@@ -9,27 +10,40 @@ beforeAll(() => {
   })
 })
 
+function createRule(rule: Partial<LintRule>): LintRule {
+  return {
+    pattern: /((GD|APP|VBLF)-[0-9]{1,5}: )/,
+    pattern_flags: 'gm',
+    target: 'title',
+    message: 'Invalid title',
+    ...rule
+  }
+}
+
+function createContext(context: Partial<PullRequestContext>): PullRequestContext {
+  return {
+    number: 1,
+    title: 'APP-1234 Change feature',
+    body: null,
+    branch: 'feature/app-1234-change-feature',
+    last_commit: 'a1aa618',
+    ...context
+  }
+}
+
 describe('Linter', () => {
   it('returns error when lint rules are not met', () => {
-    const rules: LintRule[] = [
-      {
-        pattern: /A-Z/,
-        pattern_flags: 'g',
-        target: 'title',
-        message: 'Invalid title'
-      }
-    ]
-
-    const context = {
-      number: 1,
-      title: 'APP-1234: Change feature',
-      body: null,
-      branch: 'feature/app-1234-change-feature',
-      last_commit: 'a1aa618'
-    }
+    const context = createContext({title: 'My pull request title'})
+    const rules: LintRule[] = [createRule({pattern: /((GD|APP|VBLF)-[0-9]{1,5}: )/})]
 
     const results = lint(rules, context)
-
     expect(results).toEqual(['Invalid title'])
+  })
+  it('returns NO error when lint rules are not met', () => {
+    const context = createContext({title: 'APP-1234: My feature change'})
+    const rules: LintRule[] = [createRule({pattern: /((GD|APP|VBLF)-[0-9]{1,5}: )/})]
+
+    const results = lint(rules, context)
+    expect(results).toEqual([])
   })
 })
